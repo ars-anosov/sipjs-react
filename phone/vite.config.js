2957c011dbf364b905b95089672a2ee5deb188a8
ars <ars.anosov@gmail.com>
2025-11-12 13:06:19 +0300

    Сделал ссылки относительными.

--- phone/vite.config.js
+++ phone/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // делает ссылки относительными
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  server: {
+    port: 3000,
+  },
   plugins: [react()],
 });
